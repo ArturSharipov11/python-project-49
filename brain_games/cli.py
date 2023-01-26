e51e49ff55328d1a81d24fa2aad67046f44a8df7
artur sharipov <sharipoffartur@yandex.ru>
2023-01-26 18:22:49 +0500

    hexlet

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,10 +1,14 @@
 import prompt
+
+
 def welcome_user():
-	name = prompt.string('May I have your name?')
-	print('Hello, ' + name + '!')
+    name = prompt.string('May I have your name?')
+    print('Hello, ' + name + '!')
+
 
 def main():
-	welcome_user()
+    welcome_user()
+
 
 if __name__ == '__main__':
-	main()
+    main()
