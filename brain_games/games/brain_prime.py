f149bc25b39c510eed17bf7ef6665d8cf8151ebe
artur sharipov <sharipoffartur@yandex.ru>
2023-02-15 18:36:37 +0500

    hexl

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -8,14 +8,14 @@ RULES = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 def get_question_and_answer():
     ran = randint(PRIME_MIN_VALUE, PRIME_MAX_VALUE)
-    question_and_answer = (ran, is_prime(ran))
+    question_and_answer = (ran, is_prime(ran) and 'yes' or 'no')
     return question_and_answer
 
 
 def is_prime(num):
     if num <= 1:
-        return 'no'
+        return False
     for i in range(2, num // 2):
         if num % i == 0:
-            return 'no'
-    return 'yes'
+            return False
+    return True
